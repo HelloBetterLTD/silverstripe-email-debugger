90c766b4042e45a7c8ef9dd6cdfad27755180487
Nivanka Fonseka <nivanka@silverstripers.com>
2020-05-23 16:06:00 +0530

    fixed the events

--- _config.php
+++ _config.php
@@ -2,6 +2,7 @@
 
 use SilverStripers\EmailDebugger\Mailer\Mailer;
 use SilverStripe\Control\Email\Mailer as SSMailer;
+use SilverStripe\Core\Injector\Injector;
 
 $mailer = new Mailer();
 $mailer = $mailer->setSwiftMailer($swift = new Swift_Mailer(new Swift_MailTransport()));
